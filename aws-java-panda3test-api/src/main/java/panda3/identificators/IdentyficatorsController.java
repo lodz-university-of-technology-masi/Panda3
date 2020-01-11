ae8f3e2e79c702c73af4ff088c5e8467eb1fd5b7
Michał <m.tegi@outlook.com>
2020-01-11 21:15:53 +0100

    deleted login field

--- aws-java-panda3test-api/src/main/java/panda3/identificators/IdentyficatorsController.java
+++ aws-java-panda3test-api/src/main/java/panda3/identificators/IdentyficatorsController.java
@@ -6,6 +6,6 @@ public class IdentyficatorsController {
     public static String REGION = "us-east-1";
     public static String BUCKET_NAME = "";
     public static String DESTINATION = "";
-    public static String PARTICIPANT_GROUPE = "";
+    public static String PARTICIPANT_GROUP = "Candidates";
     public static String RECRUITERS_GROUP = "Recruiters";
 }
