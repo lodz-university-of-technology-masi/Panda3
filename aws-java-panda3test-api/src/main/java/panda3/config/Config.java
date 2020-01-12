79b15e763b2e663d6dcc301a13e59f3996fd1884
Michał <m.tegi@outlook.com>
2020-01-12 23:13:41 +0100

    trigger

--- aws-java-panda3test-api/src/main/java/panda3/config/Config.java
+++ aws-java-panda3test-api/src/main/java/panda3/config/Config.java
@@ -4,7 +4,7 @@ public class Config {
     public static final String USER_POOL_ID = "us-east-1_no7qa6ygZ";
     public static String USER_POOL_NAME = "panda3-user-pool";
     public static final String REGION = "us-east-1";
-    public static final String BUCKET_NAME = "";
+    public static final String BUCKET_NAME = "p3-csv";
     public static String DESTINATION = "";
     public static final String PARTICIPANT_GROUP = "Candidates";
     public static final String RECRUITERS_GROUP = "Recruiters";
--- aws-java-panda3test-api/src/main/java/panda3/service/s3/BucketService.java
+++ aws-java-panda3test-api/src/main/java/panda3/service/s3/BucketService.java
@@ -6,8 +6,8 @@ import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import com.amazonaws.services.s3.model.GetObjectRequest;
 import com.amazonaws.services.s3.model.S3Object;
 import com.opencsv.CSVReader;
+import panda3.config.Config;
 import panda3.creators.TestCreator;
-import panda3.identificators.IdentyficatorsController;
 import panda3.model.Test;
 
 import java.io.BufferedReader;
@@ -20,7 +20,7 @@ public class BucketService {
     public BucketService() {}
 
     public Test uploadFile(String key) throws SdkClientException, IOException {
-        S3Object s3Object = s3.getObject(new GetObjectRequest(IdentyficatorsController.BUCKET_NAME, key));
+        S3Object s3Object = s3.getObject(new GetObjectRequest(Config.BUCKET_NAME, key));
         try(CSVReader csvReader = new CSVReader(new BufferedReader(new InputStreamReader(s3Object.getObjectContent())))) {
             return TestCreator.createTestCsv(csvReader);
         }
