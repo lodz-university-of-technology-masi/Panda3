ea13c1af15fda7b8da10ad92fbcde8288f555ad4
Bartek <btmikolajczyk@wp.pl>
2020-01-18 16:03:51 +0100

    new /recruiter/save

--- aws-java-panda3test-api/src/main/java/panda3/creators/RecruiterTestCreator.java
+++ aws-java-panda3test-api/src/main/java/panda3/creators/RecruiterTestCreator.java
@@ -21,8 +21,10 @@ public class RecruiterTestCreator {
 
     public static RecruiterTests addRecruiterTestJsonAddTests(String id, List<String> testId) throws IOException {
         RecruiterTests recruiterTests = new TablesMapperRecruiters().getRecruiterTest(id);
-        recruiterTests.setUserId(id);
-        recruiterTests.getTestIds().addAll(testId);
+        if(!recruiterTests.getTestIds().contains(testId)){
+            recruiterTests.setUserId(id);
+            recruiterTests.getTestIds().addAll(testId);
+        }
         return recruiterTests;
     }
 }
